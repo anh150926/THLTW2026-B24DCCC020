32e9ebe1ed273eb74752821bae3baba480cf10d7
anh150926 <leanhhv14@gmail.com>
2026-01-28 00:09:34 +0700

    init BT02

--- config/routes.ts
+++ config/routes.ts
@@ -48,6 +48,12 @@
 		icon: 'AppstoreAddOutlined',
 		component: './QuanLySanPham',
 	},
+	{
+		path: '/quan-ly-don-hang',
+		name: 'QuanLyDonHang',
+		icon: 'AppstoreAddOutlined',
+		component: './QuanLyDonHang',
+	},
 	///////////////////////////////////
 
 	// DANH MUC HE THONG
